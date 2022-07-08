fa988ebf5f3460b786b0e5d557e5858e91dd030c
AtsushiNi <atsushi9731@gmail.com>
2022-07-08 19:21:57 +0900

    calendar-adjust

--- js/ev_script.js
+++ js/ev_script.js
@@ -3,8 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,dayGridDay,listMonth'
+    },
+    navLinks: true,
     googleCalendarApiKey: 'AIzaSyAoWJyeuPyvRs3mVv8mbxAh1zDQ3WeL-0A',
     events: 'g56gfkgj6d1adk03ofcf1kh5c0@group.calendar.google.com',
+    displayEventTime: false,
+    locale: "ja",
+    height: window.innerHeight - 100,
+    businessHours: {
+      daysOfWeek: [1, 2, 3, 4, 5]
+    },
 
     eventClick: function (arg) {
       window.open(arg.event.url, 'google-calendar-event', 'width=700,height=600');
