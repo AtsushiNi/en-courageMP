69b4d08a714b34b0b3f1930b6c275e3116211068
AtsushiNi <atsushi9731@gmail.com>
2022-07-08 22:47:35 +0900

    commit

--- js/ev_script.js
+++ js/ev_script.js
@@ -19,13 +19,33 @@ document.addEventListener('DOMContentLoaded', function () {
     },
 
     eventClick: function (arg) {
-      window.open(arg.event.url, 'google-calendar-event', 'width=700,height=600');
       arg.jsEvent.preventDefault()
     },
 
     loading: function (bool) {
       document.getElementById('loading').style.display =
         bool ? 'block' : 'none';
+    },
+    eventDidMount: function (info) {
+      const description = info.event._def.extendedProps.description
+      const title = info.event._def.title
+      let url = description.match(/url:.*\n/)[0]
+      url = url.slice(4, -1)
+      let image = description.match(/image:.*/)[0]
+      image = image.slice(6)
+      const path  = "./events/" + image
+      const content = `
+        <div style="text-align: center">
+          <div>${title}</div>
+          <a href="${url}" class="tooltip-img" target="_blank">
+            <img src="${path}" style="width: 200px;">
+          </a>
+        </div>`
+      tippy(".fc-event-title", {
+        content: content,
+        trigger: "click",
+        allowHTML: true
+      })
     }
   });
 
