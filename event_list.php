d2c8ee8f21645905670f074da65fc10228912954
AtsushiNi <atsushi9731@gmail.com>
2022-07-18 13:08:45 +0900

    event-list

---
+++ event_list.php
@@ -0,0 +1,18 @@
+<?php
+  $selection_preparation = [
+    "information_session" => [
+      [
+        "path" => "https://aaa",
+        "image" => "",
+        "title" => ""
+      ]
+    ],
+    "ES" => [
+      [
+        "path" => "https://aaa",
+        "image" => "10/7/outstanding-07.jpeg",
+        "title" => "7/23・27<br>就活講座 ”Outstanding”<br>第7回「強い志望動機の作り方」"
+      ]
+    ]
+  ]
+?>
