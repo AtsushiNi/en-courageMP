a7ad57c571d261df1c952b25ec07c64f525a4cc3
AtsushiNi <atsushi9731@gmail.com>
2022-07-28 13:44:28 +0900

    drag and drop

---
+++ admin/js/self-analytics.js
@@ -0,0 +1,72 @@
+async function getOCList() {
+  const response = await $.get("../backend/get_oc_list.php")
+  const list = response.data
+  $(".event ul").empty()
+
+  list.forEach(itemData => {
+    const li = $("#li-tmp").clone(true, true)
+    li.attr("data-id", itemData.id)
+    li.css("display", "")
+    const image = itemData.image ? "../../images/events/" + itemData.image : "../no-image.png"
+    li.find("img").attr("src", image)
+
+    li.on("dragstart", onDragStart)
+    li.on("dragover", onDragOver)
+    li.on("dragend", onDragEnd)
+    li.on("drag", onDrag)
+
+    $(".event ul").append(li)
+  })
+}
+getOCList()
+
+document.addEventListener("dragover", event => {
+  event.preventDefault()
+})
+
+document.addEventListener("drop", event => {
+  event.preventDefault();
+})
+
+function onDragStart(event) {
+  event.originalEvent.dataTransfer.setData('text', $(this).data('id'))
+  event.target.classList.add("dragging")
+  event.currentTarget.style.opacity = 0
+  event.currentTarget.style.transition = "none"
+}
+
+function onDrag(event) {
+  const pageX = event.originalEvent.pageX
+  let left = []
+  $(".event ul li").each((_i,li) => left.push(li.getBoundingClientRect().left))
+  left = left.filter(value => value < pageX)
+}
+
+function onDragOver(event) {
+  event.preventDefault()
+
+  const targetElement = event.currentTarget.getBoundingClientRect()
+  const x = event.originalEvent.clientX - targetElement.left
+  if (x < ($(this).innerWidth() / 2)) {
+    $(this).css('left', '0')
+    $(this).css("transition", "all 0.5s")
+  } else {
+    $(this).css('left', - event.currentTarget.clientWidth + 'px')
+    $(this).css("transition", "all 0.5s")
+  }
+}
+
+function onDragEnd(event) {
+  event.target.classList.remove("dragging")
+
+  const pageX = event.originalEvent.pageX
+  let left = []
+  $(".event ul li").each((_i,li) => left.push(li.getBoundingClientRect().left))
+  left = left.filter(value => value < pageX)
+  this.style.opacity = 1
+  $(".event ul")[0].insertBefore(this, $(".event ul li")[left.length-1])
+  $(".event ul li").each((_i, li) => {
+    li.style.left = 0
+    li.style.transition = "none"
+  })
+}
