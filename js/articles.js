65dcc1f9b7624122a892ad103e767f633ec1b45d
AtsushiNi <atsushi9731@gmail.com>
2022-08-14 15:56:47 +0900

    article-search

--- js/articles.js
+++ js/articles.js
@@ -1,19 +1,77 @@
-async function setExperiences() {
-  const response = await $.get("../backend/get_experiences.php")
-  const experiences = response.data
+let articles = []
 
-  experiences.forEach(experience => {
-    const image = "../images/industries/" + experience.image
+async function setArticles() {
+  const response = await $.get("../backend/get_articles.php")
+  articles= response.data
+
+  articles.forEach(article => {
+    const image = "../images/industries/" + article.image
     const card = $("#card-tmp > div").clone(true, true)
     card.css("display", "")
     card.find("img").attr("src", image)
-    card.find("h5").html("<strong>"+experience.company+"</strong>")
-    card.find("p").html(new Date(experience.created_at).toLocaleDateString())
-    card.find(".badges").append('<span class="badge rounded-pill bg-secondary">' + experience.type + '</span>')
-    experience.industries.forEach(industry => {
+    card.find("h5").html("<strong>"+article.company+"</strong>")
+    card.find("p").html(new Date(article.created_at).toLocaleDateString())
+    card.find(".badges").append('<span class="badge rounded-pill bg-secondary">' + article.type + '</span>')
+    article.industries.forEach(industry => {
       card.find(".badges").append('<span class="badge rounded-pill bg-secondary">' + industry + '</span>')
     })
     $("#card-group").append(card)
   })
 }
-setExperiences()
+setArticles()
+
+function search() {
+  let showingIndexes = []
+
+  // 企業名
+  const word = $("#company-name-input").val()
+  if(!!word) {
+    const reg = new RegExp(word)
+    articles.forEach((article,index) => {
+      if(reg.test(article.company)) {
+        showingIndexes.push(index)
+      }
+    })
+  } else {
+    showingIndexes = [...Array(articles.length)].map((_,i) => i)
+  }
+
+  // タイプ
+  let checked = $(".list-group.type input:checked")
+  let unChecked = $(".list-group.type input:not(:checked)")
+  let checkedTypes = checked.map(function() { return $(this).attr("data-type")}).toArray()
+  if(checked.length && unChecked.length) {
+    showingIndexes = showingIndexes.filter(index => {
+      const article = articles[index]
+      return checkedTypes.includes(article.type)
+    })
+  }
+
+  // 業界
+  checked = $(".list-group.industry input:checked")
+  unChecked = $(".list-group.industry input:not(:checked)")
+  let checkedIndustries = checked.map(function() { return $(this).attr("data-type")}).toArray()
+  if(checked.length && unChecked.length) {
+    showingIndexes = showingIndexes.filter(index => {
+      const article = articles[index]
+      return article.industries.some(industry => checkedIndustries.includes(industry))
+    })
+  }
+
+  $("#card-group").children().each(function(index) {
+    if(showingIndexes.includes(index)) {
+      $(this).css("display", "")
+    } else {
+      $(this).css("display", "none")
+    }
+  })
+}
+
+// 企業名検索
+$("#company-name-input").on("input", function() { search() })
+
+// タイプ検索
+$(".list-group.type input").on("change", function() { search() })
+
+// 業界検索
+$(".list-group.industry input").on("change", function() { search() })
