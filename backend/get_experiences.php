2f0745065299428e89d64aeb950b34c855806b41
AtsushiNi <atsushi9731@gmail.com>
2022-08-14 13:52:40 +0900

    commiit

--- backend/get_experiences.php
+++ backend/get_experiences.php
@@ -3,6 +3,21 @@
 
   $experiences = file_get_contents("../admin/data/experience.json");
   $experiences = json_decode($experiences, true);
-  $json = json_encode($experiences, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+  $columns = file_get_contents("../admin/data/column.json");
+  $columns = json_decode($columns, true);
+
+  $articles = array("data" => array_merge($experiences["data"], $columns["data"]));
+
+  // 著者情報を隠す
+  foreach ($articles["data"] as &$data) {
+    unset($data["author"]);
+  }
+  unset($data); /* https://pasela.hatenablog.com/entry/20080527/foreach */
+
+  // ソート
+  array_multisort(array_map("strtotime", array_column($articles["data"], "created_at")), SORT_DESC, $articles["data"]);
+
+  $json = json_encode($articles, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
   echo $json;
 ?>
